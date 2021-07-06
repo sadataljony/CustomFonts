4e755c0ff77cfea325a7f0d8eb220fbe2bb9c634
sadataljony <sadataljony@gmail.com>
2021-07-07 01:08:40 +0600

    Initial commit

---
+++ app/src/main/java/com/sadataljony/app/android/customfonts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sadataljony.app.android.customfonts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
